272a138a29f8ddfd32e56a55dd2528d260226802
Peter A. Bigot <bigotp@acm.org>
2012-06-10 08:31:07 -0500

    Use packed structure to access USCI_A UART registers independent of peripheral

--- Demo/Common/drivers/MSP430/bsp430/exp430f5438/serial.c
+++ Demo/Common/drivers/MSP430/bsp430/exp430f5438/serial.c
@@ -1,16 +1,67 @@
 #include "platform.h"
 #include "serial.h"
 
+/* Register map for USCI_Ax peripheral in UART mode */
+typedef struct __attribute__((__packed__)) xUSCI_A_UART
+{
+	union {
+		unsigned int ctlw0;
+		struct {
+			unsigned char ctl1;
+			unsigned char ctl0;
+		};
+	};
+	unsigned int _unused_0x02;
+	unsigned int _unused_0x04;
+	union {
+		unsigned int brw;
+		struct {
+			unsigned char br0;
+			unsigned char br1;
+		};
+	};
+	unsigned char mctl;
+	unsigned char _reserved_0x09;
+	unsigned char stat;
+	unsigned char _reserved_0x0B;
+	unsigned char rxbuf;
+	unsigned char _reserved_0x0D;
+	unsigned char txbuf;
+	unsigned char _reserved_0x0F;
+	unsigned char abctl;
+	unsigned char _reserved_0x11;
+	union {
+		unsigned int irctl;
+		struct {
+			unsigned char irtctl;
+			unsigned char irrctl;
+		};
+	};
+	unsigned int _unused_0x14;
+	unsigned int _unused_0x16;
+	unsigned int _unused_0x18;
+	unsigned int _unused_0x1a;
+	union {
+		unsigned int ictl;
+		struct {
+			unsigned char ie;
+			unsigned char ifg;
+		};
+	};
+	unsigned int iv;
+} xUSCI_A_UART;
+
 xComPortHandle
 xSerialPortInitMinimal (unsigned long ulWantedBaud, unsigned portBASE_TYPE uxQueueLength)
 {
+	volatile xUSCI_A_UART* uca1 = (volatile xUSCI_A_UART*) __MSP430_BASEADDRESS_USCI_A1__;
+
 	/* Hold the UART in reset during configuration */
-	UCA1CTL1 |= UCSWRST;
-	UCA1CTLW0 = UCSWRST | UCSSEL__ACLK;
-	UCA1BRW = 3;
-	UCA1MCTL = (0 * UCBRF_1) | (3 * UCBRS_1);
+	uca1->ctlw0 = UCSWRST | UCSSEL__ACLK;
+	uca1->brw = 3;
+	uca1->mctl = (0 * UCBRF_1) | (3 * UCBRS_1);
 	P5SEL |= BIT6 | BIT7;
-	UCA1CTL1 &= ~UCSWRST;
+	uca1->ctlw0 &= ~UCSWRST;
 	return 0;
 }
 
@@ -47,3 +98,27 @@ void
 vSerialClose (xComPortHandle xPort)
 {
 }
+
+#if 0
+void
+checkValues ()
+{
+	volatile xUSCI_A_UART* uca1 = (volatile xUSCI_A_UART*) __MSP430_BASEADDRESS_USCI_A1__;
+#define CHECK_VAL(_s,_v) printf("Register " #_s " at %p, field at %p\n", &_s, &(uca1->_v))
+
+	printf("uca1 at %p\n", uca1);
+	CHECK_VAL(UCA1CTLW0, ctlw0);
+	CHECK_VAL(UCA1CTL0, ctl0);
+	CHECK_VAL(UCA1CTL1, ctl1);
+	CHECK_VAL(UCA1BRW, brw);
+	CHECK_VAL(UCA1BR0, br0);
+	CHECK_VAL(UCA1BR1, br1);
+	CHECK_VAL(UCA1IRCTL, irctl);
+	CHECK_VAL(UCA1IRTCTL, irtctl);
+	CHECK_VAL(UCA1IRRCTL, irrctl);
+	CHECK_VAL(UCA1ICTL, ictl);
+	CHECK_VAL(UCA1IE, ie);
+	CHECK_VAL(UCA1IFG, ifg);
+	CHECK_VAL(UCA1IV, iv);
+}
+#endif
